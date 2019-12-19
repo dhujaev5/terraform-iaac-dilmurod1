cfe3aadc31e0489c5b963f9f31d8378904fddd25
Dil <dilhujaev@gmail.com>
2019-12-18 19:41:49 -0600

    updated

--- route53.tf
+++ route53.tf
@@ -2,6 +2,6 @@ resource "aws_route53_record" "www" {
   zone_id = "Z2O0LFVAFO8Z56" 
   name    = "www.myawstestclass.com" 
   type    = "A" 
-  ttl     = "" 
+  ttl     = "60" 
   records = [aws_instance.web.public_ip]
 } 
