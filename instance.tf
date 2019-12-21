bc4548b65e4e35790eadabf1911bd558fa8cfff3
Dil <dilhujaev@gmail.com>
2019-12-21 10:53:34 -0600

    added again

--- instance.tf
+++ instance.tf
@@ -1,16 +1,22 @@
-resource "aws_instance" "web" { 
+resource "aws_instance" "web" {
   count = 10
-  ami           = "ami-00068cd7555f543d5" 
-  instance_type = "t2.micro" 
+  ami             = "ami-00068cd7555f543d5"
+  instance_type   = "t2.micro"
   associate_public_ip_address = "true"
-  key_name= aws_key_pair.deployer.key_name
+  key_name = aws_key_pair.deployer.key_name
   security_groups = ["allow_ssh"]
   user_data = file("userdata_file")
- tags = { 
-    Name = "HelloWorld${count.index + 1}" 
-
-  } 
-} 
-
-
+  
+  lifecycle{
+    prevent_destroy = false
+  }
+  tags = {
+    Name = "HelloWorld${count.index +1}"
+  }
+}
+resource "aws_instance" "imported" {
+  ami             = "ami-00068cd7555f543d5"
+  key_name        = aws_key_pair.deployer.key_name
+  security_groups = ["allow_ssh"]
+  instance_type  = "t2.micro"
 }
