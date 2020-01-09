38907aeeb271e291ed70e6d97e2df6e45d8870bd
Dil <dilhujaev@gmail.com>
2020-01-08 18:43:14 -0600

    updated error

--- data_source/ec2.tf
+++ data_source/ec2.tf
@@ -1,15 +1,15 @@
 #go to region us-east-2
-provider "aws" { 
-  region = "us-east-2" 
+provider "aws" {
+    region = "us-east-2" 
 }
 
 #search for ubuntu latest with the owner
-data "aws_ami" "ubuntu" { 
-  most_recent = true 
-  owners = ["099720109477"] 
+data "aws_ami" "ubuntu" {
+    most_recent = true
+    owners = ["099720109477"] 
 }
 
 #show the AMI id
-output "ami" { 
-  value = "${data.aws_ami.ubuntu.id}" 
+output "ami" {
+    value = "${data.aws_ami.ubuntu.id}"
 }
