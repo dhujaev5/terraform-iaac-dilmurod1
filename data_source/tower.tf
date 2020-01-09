5dc729aa9310c48d1a91cefbedc0493c441eaf20
Dil <dilhujaev@gmail.com>
2020-01-08 20:06:27 -0600

    space rem

--- data_source/tower.tf
+++ data_source/tower.tf
@@ -1,54 +1,47 @@
-#go to region us-east-2
-
-
-#search for centos latest with the owner
+# Search for CentOS latest with the owner
 data "aws_ami" "centos" {
-    filter{
+    filter {
         name = "root-device-type"
         values = ["ebs"]
-    }
+        }
     filter {
         name = "name"
         values = ["CentOS Linux 7 x86_64 HVM EBS *"]
-        }
+        } 
     most_recent = true
-    owners = ["679593333241"] 
+    owners = ["679593333241"]
 }
-
-#show the AMI id
-output "ami" {
+# Show the AMI id
+output "centos" {
     value = data.aws_ami.centos.id
 }
-
 resource "aws_key_pair" "towerkey" { 
-  key_name   = "towerkey" 
+  key_name   = "deployer" 
   public_key = file("~/.ssh/id_rsa.pub") 
-}
-
-resource "aws_instance" "web" {
-    ami           = data.aws_ami.centos.id
+} 
+resource "aws_instance" "tower" {
+  ami           = data.aws_ami.centos.id
   instance_type = "t2.micro"
   key_name = aws_key_pair.towerkey.key_name
   provisioner "remote-exec" {
-  connection {
-         host = self.public_ip
-         type  = "ssh"
-         user   = "centos"
-         private_keyb= file(var.ssh_key_location)
-         }
-        inline = [
-           "sudo yum install -y epel-release",
-           ]
-    }
-   tags = {
+      connection {
+          host = self.public_ip
+          type = "ssh"
+          user = "centos"
+          private_key = file("~/.ssh/id_rsa")
+          }
+          inline = [
+              "sudo yum install -y epel-release",
+              ]
+              }
+  tags = {
     Name = "HelloWorld"
   }
 }
-
-resource "aws_route53_record" "tower" {
-  zone_id = "ZONE_ID"
-  name = "tower.example.com"
-  type = "A"
-  ttl = "300"
-  records = ["aws_instance.web.public_ip"]
-  }
+resource "aws_route53_record" "tower" { 
+  zone_id = "Z1NSGWEPJFDNW" 
+  name    = "tower.example.com" 
+  type    = "A" 
+  ttl     = "300" 
+  records = [aws_instance.tower.public_ip] 
+}
