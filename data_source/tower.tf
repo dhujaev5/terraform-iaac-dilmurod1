e976948e741466639a2c14fd8c494f9957b81c27
Dil <dilhujaev@gmail.com>
2020-01-08 20:01:40 -0600

    space

--- data_source/tower.tf
+++ data_source/tower.tf
@@ -8,9 +8,9 @@ data "aws_ami" "centos" {
         values = ["ebs"]
     }
     filter {
-        name   =  "name"
-        values = ["CentOS Linux 7 x86_64 HVM EBS *"] 
-  }
+        name = "name"
+        values = ["CentOS Linux 7 x86_64 HVM EBS *"]
+        }
     most_recent = true
     owners = ["679593333241"] 
 }
